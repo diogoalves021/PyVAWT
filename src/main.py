d6abd6f92134608221958198339af65afc574587
Kepp0 <dsalves99@outlook.com>
2025-03-25 21:09:56 -0300

    modification of documentation and comments in the code

--- src/main.py
+++ src/main.py
@@ -5,17 +5,10 @@ from simulation import Turbine, Environment, actuatorcylinder
 from start_sim import load_config, initialize_turbine_and_environment, run_simulation
 
 def main():
-    # Carregar as configurações
     config = load_config()
-
-    # Inicializar turbinas e ambiente
     turbines, env, simulation_params, turbine_params, environment_params, r, ntheta = initialize_turbine_and_environment(config)
-
-    # Número de turbinas (pode ser 1 ou 2 conforme o config)
-    num_turbines = simulation_params["num_turbines"]
-
-    # Executar a simulação
-    run_simulation(turbines, env, simulation_params, r, ntheta, environment_params["Vinf"], num_turbines, turbine_params)
+    num_turbines = simulation_params['num_turbines']
+    run_simulation(turbines, env, simulation_params, r, ntheta, environment_params['Vinf'], num_turbines, turbine_params)
 
 if __name__ == "__main__":
     main()
